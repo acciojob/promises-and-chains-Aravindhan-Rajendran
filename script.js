bc85ecd85cd4ff107432f3931dd2265deae9a782
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-03 11:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,6 +15,6 @@ document.getElementById('btn').addEventListener('click', function(e) {
   agecheck.then(function() {
     setTimeout(function() { alert("Welcome, " + namevalue + ". You can vote.") }, 4000);
   }).catch(function(){
-    alert("Oh sorry " + namevalue+ "." + "You aren't old enough.")
+    alert("Oh sorry" + namevalue+ "." + " You aren't old enough.")
   });
 });
