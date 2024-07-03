54c2127caf8636f9d45f09e803cbc666f150709d
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-03 11:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+document.getElementById('btn').addEventListener('click', function(e) {
+  e.preventDefault(); // Prevent the form from being submitted normally
+  
+  let agevalue = Number(document.getElementById("age").value);
+  let namevalue = document.getElementById("name").value;
+
+  let agecheck = new Promise(function(resolve, reject) {
+    if(agevalue >= 18){
+      resolve();
+    } else {
+      reject();
+    }
+  });
+
+  agecheck.then(function() {
+    setTimeout(function() { alert("Welcome, " + namevalue + ". You can vote") }, 4000);
+  }).catch(function(){
+    alert("Oh sorry " + namevalue + ". You aren't old enough")
+  });
+});
